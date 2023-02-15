e9c63ab53ca934b16af5df23b1286bc0978e8f40
giisen <kristerekelund@me.com>
2023-02-15 13:41:01 +0100

    init_1

---
+++ Controllers/Herocontroller.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RobinAPI.Models;
+
+namespace RobinAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Herocontroller : ControllerBase
+    {
+        private static List<Hero> _heroes =new List<Hero>()
+        {
+            new Hero(){id = 1, Name = "Tony Stark", SuperHeroName = "Ironman",Team = "Avengers"},
+            new Hero(){id = 2, Name = "Bruce Wayne", SuperHeroName = "Läderlappen",Team = "Justice league"} 
+        };
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_heroes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok(_heroes.FirstOrDefault(x => x.id == id));
+        }
+
+        [HttpPost]
+        public IActionResult Post(Hero hero)
+        {
+            _heroes.Add(hero);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(Hero hero)
+        {
+            var heroToUpdate = _heroes.FirstOrDefault(x => x.id == hero.id);
+            if (heroToUpdate == null)
+            {
+                return NotFound();
+            }
+            heroToUpdate.Name = hero.Name;
+            heroToUpdate.Team=hero.Team;
+            heroToUpdate.SuperHeroName=hero.SuperHeroName;
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var heroToDelete=_heroes.FirstOrDefault(x => x.id == id);
+            if (heroToDelete == null)
+            {
+                return NotFound();
+            }
+
+            _heroes.Remove(heroToDelete);
+            return Ok();
+        }
+
+    }
+}
---
+++ Models/Hero.cs
@@ -0,0 +1,9 @@
+﻿namespace RobinAPI.Models;
+
+public class Hero
+{
+    public int id { get; set; }
+    public string Name { get; set; }
+    public string SuperHeroName { get; set; }
+    public string Team { get; set; }
+}
